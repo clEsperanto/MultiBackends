5cc14598b911ac840bc08d563d6ba56d818efbe1
Stephane Rigaud <Stephane.rigaud@pasteur.fr>
2023-04-22 00:39:14 +0000

    untested multi backend and device

--- include/backend.hpp
+++ include/backend.hpp
@@ -13,11 +13,24 @@ namespace cle
     class Backend
     {
     public:
+        // enum for backend types
+        enum class Type
+        {
+            CUDA,
+            OPENCL
+        };
+
         Backend() = default;
         virtual ~Backend() = default;
 
+        virtual Backend::Type getType() = 0;
         virtual std::vector<std::string> getDeviceList(const std::optional<std::string> &type) = 0;
         virtual std::vector<cle::Device> getDevices(const std::optional<std::string> &type) = 0;
+
+        virtual void allocateMemory(const Device& device, const size_t& size, void* data_ptr) = 0;
+        virtual void freeMemory(const Device& device, void* data_ptr) = 0;
+        virtual void writeMemory(const Device& device, void* data_ptr, const size_t& size, const void* host_ptr) = 0;
+        virtual void readMemory(const Device& device, const void* data_ptr, const size_t& size, void* host_ptr) = 0;
     };
 
     class CUDABackend : public Backend
@@ -26,7 +39,7 @@ namespace cle
         CUDABackend() = default;
         virtual ~CUDABackend() = default;
 
-        virtual std::vector<cle::Device> getDevices(const std::optional<std::string> &type = std::nullopt) override
+        virtual std::vector<cle::CUDADevice> getDevices(const std::optional<std::string> &type = std::nullopt) override
         {
             int deviceCount;
             cudaError_t error = cudaGetDeviceCount(&deviceCount);
@@ -34,7 +47,7 @@ namespace cle
             std::vector<cle::Device> devices;
             for (int i = 0; i < deviceCount; i++)
             {
-                devices.push_back(cle::Device(i));
+                devices.push_back(cle::CUDADevice(i));
             }
 
             return devices;
@@ -50,6 +63,64 @@ namespace cle
                 deviceList.push_back(devices[i].getName());
             }
         }
+
+        Backend::Type getType() override
+        {
+            return Backend::Type::CUDA;
+        }
+
+        void allocateMemory(const Device& device, const size_t& size, void* data_ptr) override
+        {
+            cudaError_t err = cudaSetDevice(device.getCUDeviceID());
+            if (err != cudaSuccess) {
+                throw std::runtime_error("Error: Failed to set CUDA device");
+            }
+
+            cudaError_t err = cudaMalloc(data_ptr, size);
+            if (err != cudaSuccess) {
+                throw std::runtime_error("Error: Failed to allocate CUDA memory");
+            }
+        }
+
+        void freeMemory(const Device& device, void* data_ptr) override
+        {
+            cudaError_t err = cudaSetDevice(device.getCUDeviceID());
+            if (err != cudaSuccess) {
+                throw std::runtime_error("Error: Failed to set CUDA device");
+            }
+
+            cudaError_t err = cudaFree(data_ptr);
+            if (err != cudaSuccess) {
+                throw std::runtime_error("Error: Failed to free CUDA memory");
+            }
+        }
+
+        void writeMemory(const Device& device, void* data_ptr, const size_t& size, const void* host_ptr) override
+        {
+            cudaError_t err = cudaSetDevice(device.getCUDeviceID());
+            if (err != cudaSuccess) {
+                throw std::runtime_error("Error: Failed to set CUDA device");
+            }
+
+            cudaError_t err = cudaMemcpy(data_ptr, host_ptr, size, cudaMemcpyHostToDevice);
+            if (err != cudaSuccess) {
+                throw std::runtime_error("Error: Failed to write CUDA memory");
+            }
+        }
+
+        void readMemory(const Device& device, const void* data_ptr, const size_t& size, void* host_ptr) override
+        {
+            cudaError_t err = cudaSetDevice(device.getCUDeviceID());
+            if (err != cudaSuccess) {
+                throw std::runtime_error("Error: Failed to set CUDA device");
+            }
+            
+            cudaError_t err = cudaMemcpy(host_ptr, data_ptr, size, cudaMemcpyDeviceToHost);
+            if (err != cudaSuccess) {
+                throw std::runtime_error("Error: Failed to read CUDA memory");
+            }
+        }
+
     };
 
     class OpenCLBackend : public Backend
@@ -58,7 +129,7 @@ namespace cle
         OpenCLBackend() = default;
         virtual ~OpenCLBackend() = default;
 
-        virtual std::vector<cle::Device> getDevices(const std::optional<std::string> &type = std::make_optional<std::string>("all")) override
+        virtual std::vector<cle::OpenCLDevice> getDevices(const std::optional<std::string> &type = std::make_optional<std::string>("all")) override
         {
             // Get the OpenCL platforms and devices
             std::vector<cl::Platform> platforms;
@@ -92,7 +163,7 @@ namespace cle
 
                 for (int j = 0; j < clDevices.size(); j++)
                 {
-                    devices.push_back(cle::Device(clDevices[j]));
+                    devices.push_back(cle::OpenCLDevice(clDevices[j]));
                 }
             }
 
@@ -111,7 +182,80 @@ namespace cle
 
             return deviceList;
         }
+
+        virtual Backend::Type getType() override
+        {
+            return Backend::Type::OPENCL;
+        }
+
+        void allocateMemory(const Device& device, const size_t& size, void* data_ptr) override
+        {
+            cl_int err;
+            cl::Memory* mem_ptr = reinterpret_cast<cl::Memory*>(data_ptr);
+            *mem_ptr = cl::Buffer(device.getCLContext(), CL_MEM_READ_WRITE, size, nullptr, &err);
+            if (err != CL_SUCCESS) {
+                throw std::runtime_error("Error: Failed to allocate OpenCL memory");
+            }
+        }
+
+        void freeMemory(const Device& device, void* data_ptr) override
+        {
+            cl::Memory* mem_ptr = reinterpret_cast<cl::Memory*>(data_ptr);
+            cl_int err = clReleaseMemObject(*mem_ptr);
+            if (err != CL_SUCCESS) {
+                throw std::runtime_error("Error: Failed to free OpenCL memory");
+            }
+        }
+
+        void writeMemory(const Device& device, void* data_ptr, const size_t& size, const void* host_ptr) override
+        {
+            cl_int err = device.getCLCommandQueue().enqueueWriteBuffer(*reinterpret_cast<cl::Buffer*>(data_ptr), CL_TRUE, 0, size, host_ptr);
+            if (err != CL_SUCCESS) {
+                throw std::runtime_error("Error: Failed to write OpenCL memory");
+            }
+        }
+
+        void readMemory(const Device& device, const void* data_ptr, const size_t& size, void* host_ptr) override
+        {
+            cl_int err = device.getCLCommandQueue().enqueueReadBuffer(*reinterpret_cast<cl::Buffer*>(data_ptr), CL_TRUE, 0, size, host_ptr);
+            if (err != CL_SUCCESS) {
+                throw std::runtime_error("Error: Failed to read OpenCL memory");
+            }
+        }
+    
     };
+
+    class BackendManager 
+    {
+    public:
+        static BackendManager& getInstance() {
+            static BackendManager instance;
+            return instance;
+        }
+
+        void selectBackend(bool useCUDA) {
+            if (useCUDA) {
+                backend = std::make_unique<CUDABackend>();
+            } else {
+                backend = std::make_unique<OpenCLBackend>();
+            }
+        }
+
+        Backend& getBackend() {
+            if (!backend) {
+                throw std::runtime_error("Backend not selected.");
+            }
+            return *backend;
+        }
+
+    private:
+        std::unique_ptr<Backend> backend;
+
+        BackendManager() {}
+        BackendManager(const BackendManager&) = delete;
+        BackendManager& operator=(const BackendManager&) = delete;
+    };
+
 
 } // namespace cle
 
--- include/device.hpp
+++ include/device.hpp
@@ -11,14 +11,23 @@ namespace cle
     class Device
     {
     public:
+        enum class Type 
+        {
+            CUDA,
+            OPENCL
+        };
+
         Device() = default;
         virtual ~Device() = default;
 
         virtual void initialize() = 0;
         virtual void finalize() = 0;
+        virtual void finish() = 0;
 
         virtual bool isInitialized() const = 0;
         virtual std::string getName() const = 0;
+        virtual std::string getInfo() const = 0;
+        virtual DeviceType getType() const = 0;
     };
 
     class OpenCLDevice : public Device
@@ -34,6 +43,11 @@ namespace cle
             {
                 finalize();
             }
+        }
+
+        DeviceType getType() const override
+        {
+            return Device::Type::OPENCL;
         }
 
         void initialize() override
@@ -76,6 +90,17 @@ namespace cle
             initialized = false;
         }
 
+        void finish() override
+        {
+            if (!isInitialized())
+            {
+                std::cerr << "OpenCL device not initialized" << std::endl;
+                return;
+            }
+
+            clCommandQueue.finish();
+        }
+
         bool isInitialized() const override
         {
             return initialized;
@@ -106,6 +131,45 @@ namespace cle
             return clDevice.getInfo<CL_DEVICE_NAME>();
         }
 
+
+        std::string getInfo() const override
+        {
+            std::ostringstream result;
+            std::string        version;
+            cl_device_type     type;
+            cl_uint            compute_units;
+            size_t             global_mem_size;
+            size_t             max_mem_size;
+
+            // Get device information
+            const auto & name = getName(device_pointer);
+            clDevice.getInfo(CL_DEVICE_VERSION, &version);
+            clDevice.getInfo(CL_DEVICE_TYPE, &type);
+            clDevice.getInfo(CL_DEVICE_MAX_COMPUTE_UNITS, &compute_units);
+            clDevice.getInfo(CL_DEVICE_GLOBAL_MEM_SIZE, &global_mem_size);
+            clDevice.getInfo(CL_DEVICE_MAX_MEM_ALLOC_SIZE, &max_mem_size);
+
+            // Print device information to output string
+            result << name << " (" << version << ")\n";
+            switch (type)
+            {
+                case CL_DEVICE_TYPE_CPU:
+                result << "\tType: CPU\n";
+                break;
+                case CL_DEVICE_TYPE_GPU:
+                result << "\tType: GPU\n";
+                break;
+                default:
+                result << "\tType: Unknown\n";
+                break;
+            }
+            result << "\tCompute Units: " << compute_units << '\n';
+            result << "\tGlobal Memory Size: " << (global_mem_size / 1000000) << " MB\n";
+            result << "\tMaximum Object Size: " << (max_mem_size / 1000000) << " MB\n";
+
+            return result.str();
+        }
+
     private:
         cl::Platform clPlatform;
         cl::Device clDevice;
@@ -125,6 +189,11 @@ namespace cle
             {
                 finalize();
             }
+        }
+
+        DeviceType getType() const override
+        {
+            return Device::Type::CUDA;
         }
 
         void initialize() override
@@ -167,6 +236,17 @@ namespace cle
             initialized = false;
         }
 
+        void finish() override
+        {
+            if (!isInitialized())
+            {
+                std::cerr << "CUDA device not initialized" << std::endl;
+                return;
+            }
+
+            cudaStreamSynchronize(cudaStream);
+        }
+
         bool isInitialized() const override
         {
             return initialized;
@@ -187,6 +267,21 @@ namespace cle
             cudaDeviceProp prop;
             cudaGetDeviceProperties(&prop, cudaDeviceIndex);
             return prop.name;
+        }
+
+        std::string getInfo() const override
+        {
+            std::ostringstream result;
+            cudaDeviceProp     prop;
+            cudaGetDeviceProperties(&prop, cudaDeviceIndex);
+
+            result << prop.name << " (" << prop.major << "." << prop.minor << ")\n";
+            result << "\tType: " << (prop.integrated ? "Integrated" : "Discrete") << '\n';
+            result << "\tCompute Units: " << prop.multiProcessorCount << '\n';
+            result << "\tGlobal Memory Size: " << (prop.totalGlobalMem / 1000000) << " MB\n";
+            result << "\tMaximum Object Size: " << (prop.maxMemoryAllocationSize / 1000000) << " MB\n";
+
+            return result.str();
         }
 
     private:
