8f06cad9235b0726c7c1230a579d576552c401cf
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-04-28 17:05:37 +0200

    ADD: create_like and copy

--- include/memory.hpp
+++ include/memory.hpp
@@ -14,25 +14,32 @@ namespace cle
         template <typename T>
         auto create(size_t width, size_t height, size_t depth, const DevicePtr &device) -> Array
         {
-            return Array{width, height, depth, sizeof(T), device};
+            return Array{width, height, depth, Array::toType<T>(), device};
+        }
+
+        auto create_like(const Array &arr) -> Array
+        {
+            return Array{arr.width(), arr.height(), arr.depth(), arr.dtype(), arr.device()};
         }
 
         template <typename T>
         auto push(const T *host_data, size_t width, size_t height, size_t depth, const DevicePtr &device) -> Array
         {
-            // auto arr = create<T>(width, height, depth, device);
-            // arr.write(host_data);
-            // return arr;
-            return Array{width, height, depth, sizeof(T), host_data, device};
+            return Array{width, height, depth, Array::toType<T>(), host_data, device};
         }
 
         template <typename T>
         auto pull(const Array &arr, T *host_arr) -> void
         {
-            // size = arr.width() * arr.height() * arr.depth();
-            // T *host_arr = new T[arr.width() * arr.height() * arr.depth()];
             arr.read(host_arr);
-            // host_arr = host_data;
+        }
+
+        template <typename T>
+        auto copy(const Array &src) -> const Array &
+        {
+            Array dst{src.width(), src.height(), src.depth(), Array::toType<T>(), src.device_};
+            src.copy(dst);
+            return dst;
         }
 
     } // namespace memory
