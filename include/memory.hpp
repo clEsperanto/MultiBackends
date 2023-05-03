b31b4be930ec8c08049ff5543c8f9f1b7b9db4c3
StRigaud <stephane.rigaud@pasteur.fr>
2023-05-03 23:32:05 +0200

    add const and ref

--- include/memory.hpp
+++ include/memory.hpp
@@ -12,18 +12,18 @@ namespace cle
         using DevicePtr = std::shared_ptr<cle::Device>;
 
         template <typename T>
-        auto create(size_t width, size_t height, size_t depth, const DevicePtr &device) -> Array
+        auto create(const size_t &width, const size_t &height, const size_t &depth, const DevicePtr &device) -> const Array
         {
             return Array{width, height, depth, Array::toType<T>(), device};
         }
 
-        auto create_like(const Array &arr) -> Array
+        auto create_like(const Array &arr) -> const Array
         {
             return Array{arr.width(), arr.height(), arr.depth(), arr.dtype(), arr.device()};
         }
 
         template <typename T>
-        auto push(const T *host_data, size_t width, size_t height, size_t depth, const DevicePtr &device) -> Array
+        auto push(const T *host_data, const size_t &width, const size_t &height, const size_t &depth, const DevicePtr &device) -> const Array
         {
             return Array{width, height, depth, Array::toType<T>(), host_data, device};
         }
@@ -35,9 +35,9 @@ namespace cle
         }
 
         template <typename T>
-        auto copy(const Array &src) -> const Array &
+        auto copy(const Array &src) -> const Array
         {
-            Array dst{src.width(), src.height(), src.depth(), Array::toType<T>(), src.device_};
+            Array dst{src.width(), src.height(), src.depth(), Array::toType<T>(), src.device()};
             src.copy(dst);
             return dst;
         }
