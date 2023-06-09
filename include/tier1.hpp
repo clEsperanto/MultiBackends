f41239836cc0c80f64373ff8b21f4588e15fb766
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-06-09 13:29:50 +0200

    add absolute kernel exe in t1

---
+++ include/tier1.hpp
@@ -0,0 +1,24 @@
+#ifndef __INCLUDE_TIER1_HPP
+#define __INCLUDE_TIER1_HPP
+
+#include "array.hpp"
+#include "backend.hpp"
+#include "execution.hpp"
+
+#include "cle_absolute.h"
+
+namespace cle::tier1
+{
+
+using DevicePtr = std::shared_ptr<cle::Device>;
+using ParameterMap = std::map<std::string, std::variant<Array, float, int>>;
+using ConstantMap = std::map<std::string, int>;
+using KernelInfo = std::pair<std::string, std::string>;
+using RangeArray = std::array<size_t, 3>;
+
+auto
+absolute_func(const Array & src, const Array & dst, const DevicePtr & device) -> void;
+
+} // namespace cle::tier1
+
+#endif // __INCLUDE_TIER1_HPP
---
+++ src/tier1.cpp
@@ -0,0 +1,16 @@
+#include "tier1.hpp"
+
+namespace cle
+{
+
+auto
+absolute_func(const Array & src, const Array & dst, const DevicePtr & device) -> void
+{
+  const KernelInfo   kernel_func = { "absolute", kernel::absolute };
+  const ConstantMap  constants = {};
+  const ParameterMap parameters = { { "src", src }, { "dst", dst } };
+  const RangeArray   global_range = { dst.width(), dst.height(), dst.depth() };
+  execute(device, kernel_func, parameters, constants, global_range);
+}
+
+} // namespace cle
