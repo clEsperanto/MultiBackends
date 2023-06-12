5f456a6f90188a8260b3293600e62d2fe803eddf
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-06-12 16:03:58 +0200

    add separable and gaussian

--- include/tier1.hpp
+++ include/tier1.hpp
@@ -16,7 +16,28 @@ using KernelInfo = std::pair<std::string, std::string>;
 using RangeArray = std::array<size_t, 3>;
 
 auto
+separable_func(const Array &     src,
+               const Array &     dst,
+               const float &     sigma,
+               const int &       radius,
+               const int &       dimension,
+               const DevicePtr & device) -> void;
+auto
+execute_separable_func(const Array &     src,
+                       const Array &     dst,
+                       const float &     sigma,
+                       const int &       radius,
+                       const DevicePtr & device) -> void;
+auto
 absolute_func(const Array & src, const Array & dst, const DevicePtr & device) -> void;
+auto
+gaussian_blur_func(const Array &     src,
+                   const Array &     dst,
+                   const float &     sigma_x,
+                   const float &     sigma_y,
+                   const float &     sigma_z,
+                   const DevicePtr & device) -> void;
+
 
 } // namespace cle::tier1
 
--- src/tier1.cpp
+++ src/tier1.cpp
@@ -1,6 +1,7 @@
 #include "tier1.hpp"
 
 #include "cle_absolute.h"
+#include "cle_gaussian_blur_separable.h"
 
 namespace cle::tier1
 {
@@ -8,11 +9,82 @@ namespace cle::tier1
 auto
 absolute_func(const Array & src, const Array & dst, const DevicePtr & device) -> void
 {
-  const KernelInfo    kernel_func = { "absolute", kernel::absolute };
+  const KernelInfo    kernel = { "absolute", kernel::absolute };
   const ConstantList  constants = {};
   const ParameterList parameters = { { "src", src }, { "dst", dst } };
   const RangeArray    global_range = { dst.width(), dst.height(), dst.depth() };
-  execute(device, kernel_func, parameters, constants, global_range);
+  execute(device, kernel, parameters, constants, global_range);
+}
+
+auto
+separable_func(const Array &      src,
+               const Array &      dst,
+               const float &      sigma,
+               const int &        radius,
+               const int &        dimension,
+               const KernelInfo & kernel,
+               const DevicePtr &  device) -> void
+{
+  const ParameterList parameters = {
+    { "src", src }, { "dst", dst }, { "dim", dimension }, { "N", radius }, { "s", sigma }
+  };
+  const RangeArray   global_range = { dst.width(), dst.height(), dst.depth() };
+  const ConstantList constants = {};
+  execute(device, kernel, parameters, constants, global_range);
+}
+
+auto
+execute_separable_func(const Array &                src,
+                       const Array &                dst,
+                       const std::array<float, 3> & sigma,
+                       const std::array<int, 3> &   radius,
+                       const KernelInfo &           kernel,
+                       const DevicePtr &            device) -> void
+{
+  Array tmp1(dst.width(), dst.height(), dst.depth(), dst.dtype(), dst.mtype(), dst.device());
+  Array tmp2(dst.width(), dst.height(), dst.depth(), dst.dtype(), dst.mtype(), dst.device());
+
+  if (dst.width() > 1 && sigma[0] > 0)
+  {
+    separable_func(src, tmp1, sigma[0], radius[0], 0, kernel, device);
+  }
+  else
+  {
+    src.copy(tmp1);
+  }
+  if (dst.height() > 1 && sigma[1] > 0)
+  {
+    separable_func(tmp1, tmp2, sigma[1], radius[1], 1, kernel, device);
+  }
+  else
+  {
+    tmp1.copy(tmp2);
+  }
+  if (dst.depth() > 1 && sigma[2] > 0)
+  {
+    separable_func(tmp2, dst, sigma[2], radius[2], 2, kernel, device);
+  }
+  else
+  {
+    tmp2.copy(dst);
+  }
+}
+
+auto
+gaussian_blur_func(const Array &     src,
+                   const Array &     dst,
+                   const float &     sigma_x,
+                   const float &     sigma_y,
+                   const float &     sigma_z,
+                   const DevicePtr & device) -> void
+{
+  const KernelInfo kernel = { "gaussian_blur_separable", kernel::gaussian_blur_separable };
+  execute_separable_func(src,
+                         dst,
+                         { sigma_x, sigma_y, sigma_z },
+                         { sigma2radius(sigma_x), sigma2radius(sigma_y), sigma2radius(sigma_z) },
+                         kernel,
+                         device);
 }
 
 } // namespace cle::tier1
