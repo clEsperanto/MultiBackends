87432f92e473546dd7df87c9a5bb9dfd44a9671c
Stephane Rigaud <stephane.rigaud@pasteur.fr>
2023-06-22 13:54:15 +0200

    update gaussian test

--- tests/gaussian_test.cpp
+++ tests/gaussian_test.cpp
@@ -5,6 +5,8 @@
 #include "utils.hpp"
 
 #include <assert.h>
+#include <iomanip>
+#include <limits>
 
 template <class T>
 auto
@@ -13,8 +15,8 @@ run_gaussian_blur(const cle::mType & type) -> bool
   std::cout << cle::BackendManager::getInstance().getBackend().getType() << " test (" << type << ")" << std::endl;
   auto device = cle::BackendManager::getInstance().getBackend().getDevice("TX", "all");
 
-  static const size_t w = 4;
-  static const size_t h = 4;
+  static const size_t w = 5;
+  static const size_t h = 5;
   static const size_t d = 1;
   std::vector<T>      input(w * h * d, static_cast<T>(0));
   std::vector<T>      valid(input.size(), static_cast<T>(0));
@@ -22,10 +24,20 @@ run_gaussian_blur(const cle::mType & type) -> bool
   const size_t        center = (w / 2) + (h / 2) * w + (d / 2) * h * w;
   input[center] = 100;
 
-  valid = { static_cast<T>(0.291504F), static_cast<T>(1.30643F), static_cast<T>(2.15394F), static_cast<T>(1.30643F),
-            static_cast<T>(1.30643F),  static_cast<T>(5.85502F), static_cast<T>(9.65329F), static_cast<T>(5.85502F),
-            static_cast<T>(2.15394F),  static_cast<T>(9.65329F), static_cast<T>(15.9156F), static_cast<T>(9.65329F),
-            static_cast<T>(1.30643F),  static_cast<T>(5.85502F), static_cast<T>(9.65329F), static_cast<T>(5.85502F) };
+  valid = { static_cast<T>(0.2915041744709014892578125F), static_cast<T>(1.30643117427825927734375F),
+            static_cast<T>(2.1539404392242431640625F),    static_cast<T>(1.30643117427825927734375F),
+            static_cast<T>(0.2915041744709014892578125F), static_cast<T>(1.3064310550689697265625F),
+            static_cast<T>(5.855018138885498046875F),     static_cast<T>(9.6532917022705078125F),
+            static_cast<T>(5.855018138885498046875F),     static_cast<T>(1.3064310550689697265625F),
+            static_cast<T>(2.153940677642822265625F),     static_cast<T>(9.65329265594482421875F),
+            static_cast<T>(15.91558742523193359375F),     static_cast<T>(9.65329265594482421875F),
+            static_cast<T>(2.153940677642822265625F),     static_cast<T>(1.3064310550689697265625F),
+            static_cast<T>(5.855018138885498046875F),     static_cast<T>(9.6532917022705078125F),
+            static_cast<T>(5.855018138885498046875F),     static_cast<T>(1.3064310550689697265625F),
+            static_cast<T>(0.2915041744709014892578125F), static_cast<T>(1.30643117427825927734375F),
+            static_cast<T>(2.1539404392242431640625F),    static_cast<T>(1.30643117427825927734375F),
+            static_cast<T>(0.2915041744709014892578125F) };
+
 
   cle::Array gpu_input(w, h, d, cle::toType<T>(), type, input.data(), device);
   cle::Array gpu_output(w, h, d, cle::toType<T>(), type, device);
@@ -33,17 +45,6 @@ run_gaussian_blur(const cle::mType & type) -> bool
   cle::tier1::gaussian_blur_func(gpu_input, gpu_output, 1, 1, 1, device);
 
   gpu_output.read(output.data());
-  for (auto && i : output)
-  {
-    std::cout << i << " ";
-  }
-  std::cout << std::endl;
-  for (auto && i : valid)
-  {
-    std::cout << i << " ";
-  }
-  std::cout << std::endl;
-
 
   return std::equal(output.begin(), output.end(), valid.begin()) ? 0 : 1;
 }
@@ -54,12 +55,10 @@ main(int argc, char const * argv[]) -> int
   using T = float;
 
   cle::BackendManager::getInstance().setBackend(false);
-  run_gaussian_blur<T>(cle::mType::Buffer);
-  // assert(run_gaussian_blur<T>(cle::mType::Buffer) == 0);
-  // assert(run_gaussian_blur<T>(cle::mType::Image) == 0);
+  assert(run_gaussian_blur<T>(cle::mType::Buffer) == 0);
+  assert(run_gaussian_blur<T>(cle::mType::Image) == 0);
 
   cle::BackendManager::getInstance().setBackend(true);
-  run_gaussian_blur<T>(cle::mType::Buffer);
-  // assert(run_gaussian_blur<T>(cle::mType::Buffer) == 0);
+  assert(run_gaussian_blur<T>(cle::mType::Buffer) == 0);
   // assert(run_gaussian_blur<T>(cle::mType::Image) == 0);
 }
